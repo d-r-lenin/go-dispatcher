ff0d8b65f16845c281410a4dece402f5001db6df
d-r-lenin <drlenin2002@gmail.com>
2021-01-21 18:19:44 -0800

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+const log=console.log;
+
+const express=require('express')
+const app=express();
+const server=require('http').createServer(app)
+const io=require('socket.io')(server)
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+	res.render('index.ejs');
+})
+
+
+const users={};
+
+io.on('connection',socket=>{
+	
+	log('connection opened '+socket.id)
+	
+	socket.on('im-connected',(name,i)=>{
+		users[socket.id]=name;
+		socket.broadcast.emit('user-connected',users[socket.id]);
+	})
+	
+	socket.on('disconnect',()=>{
+		socket.broadcast.emit('user-disconnected',users[socket.id])
+		log('connection closed')
+	})
+	socket.on('message',(message)=>{
+		socket.broadcast.emit('message-r',message,users[socket.id]);
+	})
+
+})
+
+
+server.listen(3000,()=>{
+	log("listaning")
+});
---
+++ public/script.js
@@ -0,0 +1,48 @@
+const name=prompt('enter your nick name')
+const msgForm=document.querySelector('.message-form')
+const sendBtn=document.querySelector('.form-button')
+const msgInput=document.querySelector('.message-input')
+const chatDisplay=document.querySelector('.message-display')
+
+
+
+
+const socket=io('ws://localhost:3000');
+
+socket.on('connect',()=>{
+	socket.on('message-r',(data,name)=>{
+		createMessage(data,name);
+	})
+	
+	createMessage('connected','you');
+	
+	socket.emit('im-connected',name);
+	
+	socket.on('user-connected',name=>{
+		createMessage('connected',name);
+	})
+	
+	socket.on('user-disconnected',name=>{
+		createMessage('disconnected',name);
+	})
+	
+	sendBtn.addEventListener('click',async()=>{
+		event.preventDefault();
+		writeMessage(msgInput.value);
+		socket.emit('message',msgInput.value);
+	})
+	
+	
+})
+
+
+function createMessage(message,name){
+	const msgEl = document.createElement('a')
+	msgEl.innerHTML=`<h1 class="recived-message" >${name}:${message}<\h1>`
+	chatDisplay.appendChild(msgEl)
+}
+function writeMessage(message){
+	const msgEl = document.createElement('a')
+	msgEl.innerHTML=`<h1 class="sent-message">you:${message}<\h1>`
+	chatDisplay.appendChild(msgEl)
+}
